fa295181114648393a21add20844502ffe537551
Bill Driscoll <bill.driscoll22@gmail.com>
2013-02-25 21:16:54 -0800

    user class with methods, class stubs, db class methods

--- src/frontend/User.java
+++ src/frontend/User.java
@@ -1,5 +1,69 @@
 package frontend;
 
-public class User {
+import java.util.*;
 
+import sql.DB;
+
+public class User {
+	private final String id;
+	private final DB db;
+	
+	public User(String user, String password, DB db){
+		this.db = db;
+		this.id = user;
+		String hash = hash(password);
+		db.addUser(user, hash);
+	}
+	
+	private String hash(String raw){
+		return raw + "a";
+	}
+	
+	public ArrayList<String> getFriends(){
+		return db.getFriends(this.id);
+	}
+	
+	public ArrayList<Message> getMessages(){
+		return db.getMessages(this.id);
+	}
+	
+	public History getHistory(){
+		return db.getHistory(this.id);
+	}
+	
+	public ArrayList<String> getAchievements(){
+		return db.getAchievements(this.id);
+	}
+	
+	public boolean isAdmin(){
+		return db.getIsAdmin(this.id);
+	}
+	
+	public void addAchievement(String achievement){
+		db.addAchievement(this.id, achievement);
+	}
+	
+	public ArrayList<Challenge> getChallenges(){
+		return db.getChallenges(this.id);
+	}
+	
+	public void addFriend(String id){
+		db.addFriend(this.id, id);
+	}
+	
+	public void removeFriend(String id){
+		db.removeFriend(this.id, id);
+	}
+	
+	public ArrayList<FriendRequest> getFriendRequests(){
+		return db.getFriendRequests(this.id);
+	}
+	
+	public void addResult(Result result){
+		db.addResult(this.id, result);
+	}
+	
+	public void setAdminStatus(boolean status){
+		db.setAdminStatus(this.id, status);
+	}
 }
--- src/sql/DB.java
+++ src/sql/DB.java
@@ -1,5 +1,72 @@
 package sql;
 
+import java.util.*;
+
+import frontend.Challenge;
+import frontend.FriendRequest;
+import frontend.History;
+import frontend.Message;
+import frontend.Result;
+
 public class DB {
+	
+	public DB(){
+		
+	}
+	
+	public void addUser(String user, String hash){
+		
+	}
+	
+	public ArrayList<String> getFriends(String userId){
+		return new ArrayList<String>();
+	}
+	
+	public ArrayList<Message> getMessages(String userId){
+		return new ArrayList<Message>();
+	}
+	
+	public History getHistory(String userId){
+		return null;
+	}
+	
+	public ArrayList<String> getAchievements(String userId){
+		return null;
+	}
+	
+	public boolean getIsAdmin(String userId){
+		return false;
+	}
+	
+	public void addAchievement(String userId, String achievement){
+		
+	}
+	
+	public ArrayList<Challenge> getChallenges(String userId){
+		return null;
+	}
+	
+	public void addFriend(String user1, String user2){
+		
+	}
+
+	public void removeFriend(String id, String id2) {
+	}
+
+	public ArrayList<FriendRequest> getFriendRequests(String id) {
+		return null;
+	}
+
+	public void addResult(String id, Result result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAdminStatus(String id, boolean status) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
