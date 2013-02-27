f02c7c0005e5f5ab138e49273a7b28e7c23fff7a
Bill Driscoll <bill.driscoll22@gmail.com>
2013-02-26 23:30:18 -0800

    fixed errors in history, message, result

--- src/frontend/Challenge.java
+++ src/frontend/Challenge.java
@@ -3,8 +3,8 @@ package frontend;
 public class Challenge extends Message {
 	private String url;
 	
-	public Challenge(String src, String dest, String body, String url){
-		super(src, dest, body);
+	public Challenge(String src, String dest, String body, String url, String time){
+		super(src, dest, body, time);
 		this.url = url;
 	}
 	
