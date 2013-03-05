cb79abbd7d11674c870e4c27bbafaa6398f9d417
Bill Driscoll <bill.driscoll22@gmail.com>
2013-03-04 20:37:35 -0800

    Addes quiz and cuqestion classes

---
+++ src/frontend/MultipleChoice.java
@@ -0,0 +1,18 @@
+package frontend;
+
+import java.util.ArrayList;
+
+public class MultipleChoice {
+	private String question;
+	private ArrayList<String> answers;
+	private int correctAnswer;
+	private int number;
+	
+	public String toHTML(){
+		return "";
+	}
+	
+	public boolean isCorrect(String answer){
+		return false;
+	}
+}
---
+++ src/frontend/Picture.java
@@ -0,0 +1,15 @@
+package frontend;
+
+public class Picture {
+	private String URL;
+	private String question;
+	private int number;
+	
+	public String toHTML(){
+		return "";
+	}
+	
+	public boolean isCorrect(String answer){
+		return false;
+	}
+}
