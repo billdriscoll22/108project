9cf13c361ff946e987bf82553eab4c8df227fc35
Bill Driscoll <bill.driscoll22@gmail.com>
2013-03-06 15:43:31 -0800

    redid mapping for quiz servlet

--- src/servlets/QuizCreateServlet.java
+++ src/servlets/QuizCreateServlet.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ import frontend.User;
 /**
  * Servlet implementation class QuizCreateServlet
  */
+@WebServlet("/QuizCreateServlet")
 public class QuizCreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
