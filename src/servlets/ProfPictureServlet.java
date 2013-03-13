adae493e2a6f660cff58937980af943c19f02527
Eli Hart <elihart@stanford.edu>
2013-03-13 14:39:01 -0700

    Fixed Profile Picture Servlet to load correctly

--- src/servlets/ProfPictureServlet.java
+++ src/servlets/ProfPictureServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import frontend.User;
 /**
  * Servlet implementation class ProfPictureServlet
  */
+@WebServlet("/ProfPictureServlet")
 public class ProfPictureServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
