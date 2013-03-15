f12cb207063bc04de47dd09a491e3b8906571ec3
Bill Driscoll <bill.driscoll22@gmail.com>
2013-03-15 01:39:19 -0700

    fixed multiple achievements bug

--- src/frontend/Achievement.java
+++ src/frontend/Achievement.java
@@ -50,31 +50,35 @@ public class Achievement {
 			int numCreated = createdQuizzes.size();
 			/*Maybe check to see if they already have it? They can delete quizzes and remake them and keep getting achievements for it.  Whatev.*/
 			if(numCreated == 1){
-				Achievement achievement = new Achievement(Achievement.AMATEUR_AUTHOR, Achievement.AMATEUR_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
-				user.addAchievement(achievement);
+				if(!alreadyHasAchievement(Achievement.AMATEUR_AUTHOR, user)){
+					Achievement achievement = new Achievement(Achievement.AMATEUR_AUTHOR, Achievement.AMATEUR_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
+					user.addAchievement(achievement);
+				}
 			}
 			else if(numCreated == 5){
-				Achievement achievement = new Achievement(Achievement.PROLIFIC_AUTHOR, Achievement.PROLIFIC_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
-				user.addAchievement(achievement);
+				if(!alreadyHasAchievement(Achievement.PROLIFIC_AUTHOR, user)){
+					Achievement achievement = new Achievement(Achievement.PROLIFIC_AUTHOR, Achievement.PROLIFIC_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
+					user.addAchievement(achievement);
+				}
 			}
 			else if(numCreated == 10){
-				Achievement achievement = new Achievement(Achievement.PRODIGIOUS_AUTHOR, Achievement.PRODIGIOUS_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
-				user.addAchievement(achievement);
+				if(!alreadyHasAchievement(Achievement.PRODIGIOUS_AUTHOR, user)){
+					Achievement achievement = new Achievement(Achievement.PRODIGIOUS_AUTHOR, Achievement.PRODIGIOUS_AUTHOR_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
+					user.addAchievement(achievement);
+				}
 			}
 		}
 		/*Need to take into account them already getting it*/
 		else if(type.equals("take")){
 			ArrayList<Result> results = user.getResults();
 			if(results.size() == 10){
-				Achievement achievement = new Achievement(Achievement.QUIZ_MACHINE, Achievement.QUIZ_MACHINE_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
-				user.addAchievement(achievement);
+				if(!alreadyHasAchievement(Achievement.QUIZ_MACHINE, user)){
+					Achievement achievement = new Achievement(Achievement.QUIZ_MACHINE, Achievement.QUIZ_MACHINE_DESCRIPTION, Achievement.ACHIEVEMENT_URL);
+					user.addAchievement(achievement);
+				}
 			}
-			ArrayList<Achievement> currentAchievements = user.getAchievements();
-			boolean hasAchievement = false;
-			for(Achievement a : currentAchievements){
-				if(a.getName().equals(Achievement.I_AM_THE_GREATEST))
-					hasAchievement = true;
-			}
+			
+			boolean hasAchievement = alreadyHasAchievement(Achievement.I_AM_THE_GREATEST, user);
 			if(!hasAchievement){
 				ArrayList<Result> quizResults = db.getTopResults(quiz.getQuizId(), 1);
 				if(quizResults.size() != 0){
@@ -85,5 +89,15 @@ public class Achievement {
 				}
 			}
 		}
+	}
+	
+	private static boolean alreadyHasAchievement(String achievementName, User user){
+		ArrayList<Achievement> currentAchievements = user.getAchievements();
+		boolean returnValue = false;
+		for(Achievement a : currentAchievements){
+			if(a.getName().equals(achievementName))
+				returnValue = true;
+		}
+		return returnValue;
 	}
 }
